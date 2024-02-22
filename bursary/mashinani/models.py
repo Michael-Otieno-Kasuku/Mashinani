996b067d339cfbc974c021dc4b6a3fa5564cac21
Michael Otieno Kasuku <michaelotienokasuku@gmail.com>
2024-02-22 12:17:08 +0300

    models modified

--- bursary/mashinani/models.py
+++ bursary/mashinani/models.py
@@ -1,65 +1,72 @@
 from django.db import models
 
 class Bank(models.Model):
-    bank_name = models.CharField(max_length=200, primary_key=True, help_text="Enter a valid Bank Name")
+    bank_id = models.AutoField(primary_key=True)
+    bank_name = models.CharField(max_length=200, unique=True, help_text="Enter a valid Bank Name")
 
     def __str__(self):
-        return f"{self.bank_name}"
+        return self.bank_name
 
 class Institution(models.Model):
-    institution_name = models.CharField(max_length=255, primary_key=True,help_text="Enter a valid Institution Name")
+    institution_id = models.AutoField(primary_key=True)
+    institution_name = models.CharField(max_length=255, unique=True,help_text="Enter a valid Institution Name")
 
     def __str__(self):
-        return f"{self.institution_name}"
+        return self.institution_name
 
-class InstitutionAccount(models.Model):
-    institution_account_number = models.CharField(max_length=200, primary_key=True, help_text="Enter a valid Institution Account Number", blank=True)
-    institution_name = models.ForeignKey(Institution, on_delete=models.CASCADE, help_text="Enter a valid Institution Name")
-    bank_name = models.ForeignKey(Bank, on_delete=models.CASCADE, help_text="Enter a valid Bank Name")
+class Account(models.Model):
+    account_id = models.AutoField(primary_key=True)
+    institution_id = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    bank_id = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=200, unique=True, help_text="Enter a valid Account Number")
 
     def __str__(self):
-        return f"{self.institution_account_number} {self.institution_name} {self.bank_name}"
+        return self.account_number
 
 class Constituency(models.Model):
-    constituency = models.CharField(max_length=255, primary_key=True, help_text="Enter the constituency")
+    constituency_id = models.AutoField(primary_key=True)
+    constituency_name = models.CharField(max_length=255, unique=True, help_text="Enter the constituency name")
 
     def __str__(self):
-        return f"{self.constituency}"
+        return self.constituency_name
 
-class VoterRegister(models.Model):
-    national_id = models.CharField(max_length=200, primary_key=True, help_text="Enter a valid National ID Number")
-    constituency = models.ForeignKey(Constituency, on_delete=models.CASCADE, help_text="Enter the Constituency")
+class Voter(models.Model):
+    voter_id = models.AutoField(primary_key=True)
+    constituency_id = models.ForeignKey(Constituency, on_delete=models.CASCADE)
+    national_id_no = models.CharField(max_length=200, unique=True, help_text="Enter a valid National ID Number")
 
     def __str__(self):
-        return f"{self.national_id} {self.constituency}"
+        return self.national_id_no
 
-class StudentRegister(models.Model):
-    registration_number = models.CharField(max_length=200, primary_key=True, help_text="Enter a valid Student Registration Number")
-    national_id = models.ForeignKey(VoterRegister, on_delete=models.CASCADE, help_text="Enter a valid National ID Number")
-    institution_name = models.ForeignKey(Institution, on_delete=models.CASCADE, help_text="Enter a Valid Institution")
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    voter_id = models.ForeignKey(VoterRegister, on_delete=models.CASCADE)
+    institution_id = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    registration_number = models.CharField(max_length=200, unique=True, help_text="Enter a valid Student Registration Number")
     first_name = models.CharField(max_length=255, help_text="Enter the first name")
-    middle_name = models.CharField(max_length=255, help_text="Enter the middle name")
     last_name = models.CharField(max_length=255, help_text="Enter the last name")
 
     def __str__(self):
-        return f"{self.registration_number} {self.national_id} {self.institution_name} {self.first_name} {self.middle_name} {self.last_name}"
+        return self.registration_number
 
 class FinancialYear(models.Model):
-    financial_year = models.CharField(max_length=200, primary_key=True, help_text="Enter a valid Financial Year")
+    financial_year_id = models.AutoField(primary_key=True)
+    financial_year = models.CharField(max_length=200, unique=True, help_text="Enter a valid Financial Year")
 
     def __str__(self):
-        return f"{self.financial_year}"
+        return self.financial_year
 
 class BursaryApplication(models.Model):
-    serial_number = models.CharField(max_length=200, primary_key=True, help_text="Auto-generated serial number")
-    national_id = models.ForeignKey(VoterRegister, on_delete=models.CASCADE, help_text="Enter a valid National ID Number")
-    registration_number = models.ForeignKey(StudentRegister, on_delete=models.CASCADE, help_text="Enter a valid Student Registration Number")
-    institution_name = models.ForeignKey(Institution, on_delete=models.CASCADE, help_text="Enter a valid Institution Name")
-    institution_account_number = models.ForeignKey(InstitutionAccount, on_delete=models.CASCADE, help_text="Enter a valid Institution Account Number")
-    financial_year = models.ForeignKey(FinancialYear, on_delete=models.CASCADE, help_text="Enter a valid Financial Year")
+    bursary_application_id = models.AutoField(primary_key=True)
+    voter_id = models.ForeignKey(Voter, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    institution_id = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    financial_year_id = models.ForeignKey(FinancialYear, on_delete=models.CASCADE)
+    serial_number = models.CharField(max_length=200, unique=True, help_text="Auto-generated serial number")
     date_submitted = models.DateTimeField(auto_now_add=True, help_text="Date of submission")
     amount_disbursed = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, help_text="Amount disbursed in Ksh")
     date_disbursed = models.DateTimeField(null=True, blank=True, help_text="Date of disbursement")
 
     def __str__(self):
-        return f"{self.serial_number} {self.national_id} {self.registration_number} {self.institution_name} {self.institution_account_number} {self.financial_year} {self.date_submitted} {self.amount_disbursed} {self.date_disbursed}"
+        return self.serial_number
