c575ec28d9707efa7c2f9f6503bb9f7593a7d4d5
Michael Otieno Kasuku <michaelotienokasuku@gmail.com>
2024-03-02 21:49:51 +0300

    prepare for version 3

--- bursary/mashinani/views.py
+++ bursary/mashinani/views.py
@@ -35,27 +35,27 @@ class ApplicationFormView(View):
             account_number = form.cleaned_data['account_number']
             financial_year_id = form.cleaned_data['financial_year_id']
             
-            # Check for existing application based on the id number and financial year i.e you can only apply once in every financial year
+            # REQ-1: Check for existing application based on the id number and financial year i.e you can only apply once in every financial year
             if BursaryApplication.objects.filter(national_id_no=national_id_no, financial_year_id=financial_year_id).exists():
                 form.add_error(None, "You have already applied bursary for this financial year!")
                 return render(request, self.template_name, {'form': form})
             
-            #Check if the id number provided is indeed belongs to that particular student
+            # REQ-2: Check if the id number provided is indeed belongs to that particular student
             if not Student.objects.filter(national_id_no=national_id_no,registration_number=registration_number).exists():
                 form.add_error(None, "You have provided a wrong registration number or national id number!")
                 return render(request, self.template_name, {'form': form})
             
-            # Check student registration i.e if the applicant is indeed a student of the given institution based on the reg no and institution id
+            # REQ-3: Check student registration i.e if the applicant is indeed a student of the given institution based on the reg no and institution id
             if not Student.objects.filter(institution_id=institution_id, registration_number=registration_number).exists():
                 form.add_error(None, "You have chosen a wrong institution or you have provided a wrong registration number!")
                 return render(request, self.template_name, {'form': form})
 
-            # Check voter eligibility based on the id number and constituency i.e you can only apply if you're a voter in Kisumu West, that is Kisumu West is the only Constituency that is in  the system
+            # REQ-4 Check voter eligibility based on the id number and constituency i.e you can only apply if you are a voter in the chosen constituency
             if not Voter.objects.filter(national_id_no=national_id_no, constituency_id=constituency_id).exists():
                 form.add_error(None, "You have entered a wrong national id number or you have entered a wrong constituency name!")
                 return render(request, self.template_name, {'form': form})
             
-            # Check if the provided account number is correct i.e the account number provided should belong to that particular institution that the user entered
+            # REQ-5 Check if the provided account number is correct i.e the account number provided should belong to that particular institution that the user entered
             if not Account.objects.filter(institution_id=institution_id, account_number=account_number).exists():
                 form.add_error(None, "You have entered a wrong accounter number or you have chosen the wrong institution")
                 return render(request, self.template_name, {'form': form})
