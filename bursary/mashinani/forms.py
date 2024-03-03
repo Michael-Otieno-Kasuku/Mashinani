c1ddbe60b7a97563462f2f3662157f6f251d1e3b
Michael Otieno Kasuku <michaelotienokasuku@gmail.com>
2024-03-03 11:08:44 +0300

    place holder text added

--- bursary/mashinani/forms.py
+++ bursary/mashinani/forms.py
@@ -1,24 +1,21 @@
-from django import forms
-from .models import BursaryApplication, Institution, FinancialYear, Constituency, Account
-
 class ApplicationForm(forms.ModelForm):
     institution_id = forms.ModelChoiceField(
         queryset=Institution.objects.all(),
         required=True,
         label='Institution Name',
-        widget=forms.Select(attrs={'class': 'blue-input-box'}),
+        widget=forms.Select(attrs={'class': 'blue-input-box', 'placeholder': 'Select an institution'}),
     )
     financial_year_id = forms.ModelChoiceField(
         queryset=FinancialYear.objects.all(),
         required=True,
         label='Financial Year',
-        widget=forms.Select(attrs={'class': 'blue-input-box'}),
+        widget=forms.Select(attrs={'class': 'blue-input-box', 'placeholder': 'Select a financial year'}),
     )
     constituency_id = forms.ModelChoiceField(
         queryset=Constituency.objects.all(),
         required=True,
         label='Constituency',
-        widget=forms.Select(attrs={'class': 'blue-input-box'}),
+        widget=forms.Select(attrs={'class': 'blue-input-box', 'placeholder': 'Select a constituency'}),
     )
 
     class Meta:
@@ -33,9 +30,9 @@ class ApplicationForm(forms.ModelForm):
             'financial_year_id': 'Financial Year',
         }
         widgets = {
-            'national_id_no': forms.TextInput(attrs={'class': 'blue-input-box'}),
-            'registration_number': forms.TextInput(attrs={'class': 'blue-input-box'}),
-            'account_number': forms.TextInput(attrs={'class': 'blue-input-box'}),
+            'national_id_no': forms.TextInput(attrs={'class': 'blue-input-box', 'placeholder': 'Enter national ID number'}),
+            'registration_number': forms.TextInput(attrs={'class': 'blue-input-box', 'placeholder': 'Enter registration number'}),
+            'account_number': forms.TextInput(attrs={'class': 'blue-input-box', 'placeholder': 'Enter account number'}),
         }
 
     def __init__(self, *args, **kwargs):
@@ -44,4 +41,3 @@ class ApplicationForm(forms.ModelForm):
         # Correctly set the initial value based on the existing instance
         if self.instance and hasattr(self.instance, 'institution_id') and self.instance.institution_id:
             self.fields['institution_id'].initial = self.instance.institution.institution_id
-    
